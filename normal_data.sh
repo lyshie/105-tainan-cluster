126d3128515d2c695b6309a1c638cae7958b45b6
SHIE, Li-Yi <lyshie@mail.nsysu.edu.tw>
2016-07-17 00:05:07 +0800

    Add comments

--- normal_data.sh
+++ normal_data.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# 取得台南市政府開放資料，轉為 school_id 當索引的 Hash Table
 wget -q -O - http://odata.tn.edu.tw/schoolapi/api/getdata | ./json_conv.pl > schools.json
 
+# PDF 轉為機器方便處理的格式 (dnf install python-pdfminer)
+# http://www.unixuser.org/~euske/python/pdfminer/
 pdf2txt -t tag 105tainan.pdf > 105tainan.tag
 
+# 正規化資料，並轉成 JSON 格式
 echo "var t = [];" > "targets.json"
 
 for i in {1..8}; do
